091d1bd9abb41e8bbcd9764ecbd6a773911a8068
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-04-07 22:18:23 +0800

    managing state on both class and functional components

--- src/classForm.js
+++ src/classForm.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import "./form.css";
 
-const ClassForm = () => {
-  return (
-    <section>
-      <form className="form">
-        <h1>Class form</h1>
-        <input className="input" placeholder="name..." type="text"></input>
-        <input className="input" placeholder="email..." type="email"></input>
-        <button>submit</button>
-      </form>
-    </section>
-  );
-};
+class ClassForm extends Component {
+  
+  state = {
+    name: "",
+    email: "",
+  };
+
+  onChangeHandlerName = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  onChangeHandlerEmail = (e) => { 
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  render() {
+    const { name, email } = this.state;
+
+    return (
+      <section>
+        <form className="form">
+          <h1>Class form</h1>
+          <input
+            className="input"
+            placeholder="name..."
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.onChangeHandlerName}
+          ></input>
+          <input
+            className="input"
+            placeholder="email..."
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.onChangeHandlerEmail}
+          ></input>
+          <button>submit</button>
+        </form>
+      </section>
+    );
+  }
+}
 
 export default ClassForm;
--- src/hooksForm.js
+++ src/hooksForm.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./form.css";
 
 const HooksFrom = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onChangeHandlerName = e => {
+    setName(e.target.value)
+  }
+  const onChangeHandlerEmail = e => { 
+    setEmail(e.target.value)
+  }
+  
   return (
     <section>
       <form className="form">
         <h1>Hooks form</h1>
-        <input className="input" placeholder="name..."></input>
-        <input className="input" placeholder="email..."></input>
+        <input
+          className="input"
+          placeholder="name..."
+          value={name}
+          onChange={onChangeHandlerName}
+        ></input>
+        <input
+          className="input"
+          placeholder="email..."
+          value={email}
+          onChange={onChangeHandlerEmail}
+        ></input>
         <button>submit</button>
       </form>
     </section>
